4be3dc9674a7b046c91ab1c7b9bd7454210d8a69
samuelsih <samuel.205025@mhs.its.ac.id>
2022-08-15 03:06:20 +0700

    add graceful shutdown

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/httprate"
+	"github.com/go-redis/redis/v8"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/samuelsih/goth/business"
 	"github.com/samuelsih/goth/db"
 	midd "github.com/samuelsih/goth/middleware"
@@ -61,5 +68,70 @@ func main() {
 	r.MethodNotAllowed(MethodNotAllowed)
 	r.NotFound(NotFound)
 
-	http.ListenAndServeTLS(":80", "example.crt", "example.key", r)
+	server := &http.Server{
+		Addr:         ":80",
+		Handler:      r,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	go func ()  {
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+
+		<- quit
+
+		closedServerChan := make(chan bool, 1)
+		closedPgxChan := make(chan bool, 1)
+		closedRedisChan := make(chan bool, 1)
+
+		go shutdownServer(closedServerChan, ctx, server)
+		go closePgx(closedPgxChan, pgDB)
+		go closeRedis(closedRedisChan, redisDB)
+
+		<- closedPgxChan
+		<- closedRedisChan
+		<- closedServerChan
+
+		if d := ctx.Done(); d != nil {
+			log.Println("Context Done Hit!")
+		}
+
+		close(closedPgxChan)
+		close(closedRedisChan)
+		close(closedServerChan)
+	}()
+
+	server.ListenAndServeTLS("example.crt", "example.key")
+}
+
+func shutdownServer(done chan bool, ctx context.Context, server *http.Server) {
+	log.Println("Shutdown server")
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server shutdown error:", err)
+	}
+
+	done <- true
+}
+
+func closePgx(done chan bool, conn *pgxpool.Pool) {
+	log.Println("Closing postgres")
+	
+	conn.Close()
+
+	done <- true
+}
+
+func closeRedis(done chan bool, conn *redis.Client) {
+	log.Println("Closing redis")
+
+	if err := conn.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	done <- true
 }
